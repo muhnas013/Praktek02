bd617ed0286c00a9a3aa2659d7d689b45080ea06
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-25 22:57:02 +0800

    Membuat Rectangle dan Behavior

--- Praktek02/src/main/java/pbo/praktek02/Rectangle.java
+++ Praktek02/src/main/java/pbo/praktek02/Rectangle.java
@@ -5,5 +5,13 @@
 package pbo.praktek02;
 
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo () {
+        System.out.println("==========================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("==========================");
+    }
 }
