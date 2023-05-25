d181a5d249971e87c3f48b6d331e144013fe8d6e
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-25 22:46:53 +0800

    Membuat Class Baru

--- Praktek02/src/main/java/pbo/praktek02/Rectangle.java
+++ Praktek02/src/main/java/pbo/praktek02/Rectangle.java
@@ -1,12 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package pbo.praktek02;
 
-public class Praktek02 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Rectangle {
+    
 }
