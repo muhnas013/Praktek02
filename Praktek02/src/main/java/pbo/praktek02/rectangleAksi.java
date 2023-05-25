c599dc4d74a98aa895a40da10f703d6e54b120df
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-25 23:00:27 +0800

    Membuat Class Aksi dan Method Main

---
+++ Praktek02/src/main/java/pbo/praktek02/rectangleAksi.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo.praktek02;
+
+public class rectangleAksi {
+    public static void main(String[] args) {
+        
+    }
+}
